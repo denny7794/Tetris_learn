68019bcd5359594910a10f3f36cfa5adbaad100d
denny7794 <denny7794@gmail.com>
2016-10-21 11:46:47 +0400

    class Block

--- src/GameTetris.java
+++ src/GameTetris.java
@@ -107,11 +107,11 @@ public class GameTetris {
     class Figure {
 
         boolean isTouchGround() {
-
+            return false;
         }
 
         boolean isCrossGround() {
-
+            return false;
         }
 
         void leaveOnTheGround() {
@@ -137,7 +137,23 @@ public class GameTetris {
     }
 
     class Block {
+        private int x, y;
 
+        public Block(int x, int y) {
+            setX(x);
+            setY(y);
+        }
+
+        public void setX(int x) { this.x = x; }
+        public void setY(int y) { this.y = y; }
+
+        int getX() { return x; }
+        int getY() { return y; }
+
+        void paint(Graphics g, int color) {
+            g.setColor(new Color(color));
+            g.drawRoundRect(x*BLOCK_SIZE+1, y*BLOCK_SIZE+1, BLOCK_SIZE-2, BLOCK_SIZE-2, ARC_RADIUS, ARC_RADIUS);
+        }
     }
 
     public class Canvas extends JPanel {
