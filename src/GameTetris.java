a3e6adc19db860c8cb98dff0deb119dc1edfde41
denny7794 <denny7794@gmail.com>
2016-10-21 10:14:36 +0400

    imports

--- src/GameTetris.java
+++ src/GameTetris.java
@@ -2,6 +2,19 @@
  * Learn classic game of Tetris
  * Created by Denis on 21.10.2016.
  */
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.util.*;
+
 public class GameTetris {
-    
+
+    public static void main(String[] args) {
+        new GameTetris().go();
+    }
+
+    void go() {
+
+    }
 }
